e6e9dc50eeead2814427c81fa00c6a13d72b1772
Brooke Bryan <brooke@bajb.net>
2020-09-09 11:08:15 +0100

    Context cookies

---
+++ src/Cookie/ContextCookie.php
@@ -0,0 +1,49 @@
+<?php
+namespace Packaged\Context\Cookie;
+
+use Packaged\Context\ContextAware;
+use Packaged\Context\ContextAwareTrait;
+use Packaged\Context\WithContext;
+use Packaged\Context\WithContextTrait;
+
+abstract class ContextCookie implements ContextAware, WithContext
+{
+  use ContextAwareTrait;
+  use WithContextTrait;
+
+  /**
+   * @var string|null
+   */
+  private $_rawValue;
+
+  abstract public function name(): string;
+
+  abstract public function ttl(): int;
+
+  public function exists(bool $checkQueued = true)
+  {
+    return $this->getContext()->cookies()->has($this->name(), $checkQueued);
+  }
+
+  public function read(bool $checkQueued = true)
+  {
+    return $this->_setRawValue($this->getContext()->cookies()->read($this->name(), $checkQueued));
+  }
+
+  protected function _getRawValue(): ?string
+  {
+    return $this->_rawValue;
+  }
+
+  protected function _setRawValue(string $value)
+  {
+    $this->_rawValue = $value;
+    return $this;
+  }
+
+  public function store()
+  {
+    $this->getContext()->cookies()->store($this->name(), $this->_getRawValue(), $this->ttl());
+    return $this;
+  }
+}
